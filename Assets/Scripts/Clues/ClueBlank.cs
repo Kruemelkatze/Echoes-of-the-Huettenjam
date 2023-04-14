da63ed3f05b2aec8ae211a92ab295b6a71dff78c
Fabian Schober <fabian@dirtypaws.studio>
2023-04-14 18:40:28 +0200

    Draggable Cues

--- Assets/Scripts/Clues/ClueBlank.cs
+++ Assets/Scripts/Clues/ClueBlank.cs
@@ -1,9 +1,13 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ClueBlank : MonoBehaviour
+public class ClueBlank : MonoBehaviour, IDropHandler
 {
     [SerializeField] private string shouldBeText = "Clueless";
     [SerializeField] private DraggableClue installedClue;
+
+    [SerializeField] private DraggableClue quicklySetCorrectClue;
 
     public bool IsFulfilled()
     {
@@ -15,13 +19,47 @@ public class ClueBlank : MonoBehaviour
         return text1?.ToLower().Replace(" ", "").Trim() == text2?.ToLower().Replace(" ", "").Trim();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnValidate()
     {
+        if (!quicklySetCorrectClue)
+            return;
+
+        shouldBeText = quicklySetCorrectClue.Text;
+        quicklySetCorrectClue = null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrop(PointerEventData eventData)
     {
+        if (eventData.pointerDrag == null)
+            return;
+
+        var draggableClue = eventData.pointerDrag.GetComponent<DraggableClue>();
+        if (draggableClue == null)
+            return;
+
+        if (installedClue == draggableClue)
+        {
+            installedClue.IgnoreDrop = true;
+        }
+
+        if (installedClue != null)
+        {
+            installedClue.ResetPosition();
+            installedClue = null;
+        }
+
+        draggableClue.RemoveFromPreviousBlank();
+        draggableClue.InstalledIn = this;
+        installedClue = draggableClue;
+        draggableClue.PositioningHandled = true;
+
+        var rectTransform = draggableClue.GetComponent<RectTransform>();
+        rectTransform.position = GetComponent<RectTransform>().position;
+    }
+
+    public void Remove(DraggableClue clue)
+    {
+        if (installedClue == clue)
+            installedClue = null;
     }
 }
